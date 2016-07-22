5dbac78767de952d059e2e8ced2d0c7edac05423
ascherer1993 <ascherer1993@gmail.com>
2016-07-22 00:26:34 -0500

    Added home controller

---
+++ NCAAFootballPlayoffs/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NCAAFootballPlayoffs.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        //https://github.com/ascherer1993/NCAAFootballPlayoffs.git
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

5e8becd5d99ba71ffba2701a80ae52eb9c7bf649
ascherer1993 <ascherer1993@gmail.com>
2016-11-05 16:22:21 -0500

    Finished general style of bracket page

--- NCAAFootballPlayoffs/Models/KnockoutModels/Bracket/BracketViewModel.js
+++ NCAAFootballPlayoffs/Models/KnockoutModels/Bracket/BracketViewModel.js
@@ -1,11 +1,12 @@
 ﻿function BracketViewModel(urls) {
     var self = this;
     self.isLoading = ko.observable(true);
-    self.games = null;
+    self.games = ko.observableArray();
 
     self.loadGames = function () {
         $.get("getGamesJSon", function (data) {
             self.games = ko.mapping.fromJSON(data);
+            ko.applyBindings(self);
         });
     }
 }
